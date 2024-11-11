3f4c5926788c81bac7f96616cace51d16b4c7830
Hsbronew <pradeepkumara4467@gmail.com>
2024-11-11 16:56:25 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "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"
 const BOT_NUMBER = "94766372136"
-const SESSION_ID = "PRABATH-MD~uTYRTRKD#brRcd-e3EWt56w7xa858V7iEoL4-GmVdEt1hUXou6aM"
+const SESSION_ID = "PRABATH-MD~SX5UjCZR#Hw_nkogi7n1SWF9_Swe9M2xnxGy8prUuPYAnG9XQcgs"
 
 
 module.exports = {
